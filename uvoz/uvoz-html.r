71b9ccc4f2bbe8e82d88342d6568c07995463645
Timotej Vesel <timotej.vesel@gmail.com>
2017-12-14 09:56:16 +0100

    Spremenil tabele, dopolnil poročilo

--- uvoz/uvoz-html.r
+++ uvoz/uvoz-html.r
@@ -5,32 +5,37 @@ stran <- html_session(link) %>% read_html()
 tabele_html <- stran %>% html_nodes(xpath="//table[@class='data-table1']")
 tabele <- lapply(tabele_html[2:40], html_table)
 leta <- sapply(tabele, . %>% .[1,1]) %>% strapplyc("([0-9]+)") %>% unlist() %>% parse_integer()
-draft_ekipe <- lapply(1:length(tabele), . %>% { mutate(tabele[[.]][-c(1, 2), ], Leto = leta[.]) }) %>% bind_rows()
+draft.ekipe <- lapply(1:length(tabele), . %>% { mutate(tabele[[.]][-c(1, 2), ], Leto = leta[.]) }) %>% bind_rows()
 
-colnames(draft_ekipe) <- c("Krog", "Izbor", "Ime", "Pozicija", "College", "Ekipa", "Leto")
+colnames(draft.ekipe) <- c("Krog", "Izbor", "Ime", "Pozicija", "College", "Ekipa", "Leto")
 
-draft_ekipe$Krog <- parse_integer(draft_ekipe$Krog)
-draft_ekipe$Izbor <- parse_integer(draft_ekipe$Izbor)
-draft_ekipe$Pozicija <- NULL
-draft_ekipe <- draft_ekipe[,-c(1:2,6)]
+draft.ekipe$Krog <- parse_integer(draft.ekipe$Krog)
+draft.ekipe$Izbor <- parse_integer(draft.ekipe$Izbor)
+draft.ekipe$Pozicija <- NULL
+draft.ekipe <- draft.ekipe[,-c(1:2,6)]
 
 
-write_csv(draft_ekipe, "podatki/draft-ekipe.csv")
+write_csv(draft.ekipe, "podatki/draft-ekipe.csv")
 
 # CSV
-uvozi.statistiko <- function()
+uvozi.statistiko <- function() {
   statistika <- read_csv("podatki/pro-football-reference.csv",
-                         na = ":", locale = locale(encoding="UTF-8"))[,c(2:5, 11:12, 14:15, 18:25)]
+                         na = ":", locale = locale(encoding="UTF-8"))[,c(2:5, 11:12,15, 18:25)]
 
-colnames(statistika) <- c("Leto", "Krog", "Izbor", "Ime", "All-Pro", "Pro Bowl",
-                          "Povprecje", "St_tekem", "Uspesne_podaje", "Podaje",
-                          "Jardi podaje", "Podaje za TD", "Prestrezene podaje",
-                          "St_tekov","Jardi_tek", "TD_tek")
+  colnames(statistika) <- c("Leto", "Krog", "Izbor", "Ime", "All-Pro", "Pro Bowl",
+                          "St.tekem", "Uspesne.p", "Podaje",
+                          "Jardi.podaje", "Podaje.TD", "Prestrezene.p",
+                          "St.tekov","Jardi.tek", "TD.tek")
 
-write_csv(statistika, "podatki/statistika.csv")
+
+
+  write_csv(statistika, "podatki/statistika.csv")
+  
+}
+  
 
 # 
-skupna <- merge(statistika, draft_ekipe, by = "Ime")
-
+skupna <- merge(statistika, draft.ekipe, by = "Ime")
+skupna <- skupna[, c(1:2, 16:17, 3:15)]
 
 
