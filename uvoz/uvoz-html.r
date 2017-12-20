093a118489af0346f8b2aedfc5c44942d4616abd
Timotej Vesel <timotej.vesel@gmail.com>
2017-12-20 19:51:28 +0100

    Spremenil razpredelnice v tidy data.

--- uvoz/uvoz-html.r
+++ uvoz/uvoz-html.r
@@ -3,7 +3,7 @@ stran <- html_session(link) %>% read_html()
 
 # HTML
 tabele_html <- stran %>% html_nodes(xpath="//table[@class='data-table1']")
-tabele <- lapply(tabele_html[2:40], html_table)
+tabele <- lapply(tabele_html[2:40], html_table)rm
 leta <- sapply(tabele, . %>% .[1,1]) %>% strapplyc("([0-9]+)") %>% unlist() %>% parse_integer()
 draft.ekipe <- lapply(1:length(tabele), . %>% { mutate(tabele[[.]][-c(1, 2), ], Leto = leta[.]) }) %>% bind_rows()
 
@@ -12,29 +12,35 @@ colnames(draft.ekipe) <- c("Krog", "Izbor", "Ime", "Pozicija", "College", "Ekipa
 draft.ekipe$Krog <- parse_integer(draft.ekipe$Krog)
 draft.ekipe$Izbor <- parse_integer(draft.ekipe$Izbor)
 draft.ekipe$Pozicija <- NULL
-draft.ekipe <- draft.ekipe[,-c(1:2,6)]
-
-
-write_csv(draft.ekipe, "podatki/draft-ekipe.csv")
+draft.ekipe <- draft.ekipe[,c(3,6,1:2,4:5)]
 
 # CSV
 uvozi.statistiko <- function() {
   statistika <- read_csv("podatki/pro-football-reference.csv",
                          na = ":", locale = locale(encoding="UTF-8"))[,c(2:5, 11:12,15, 18:25)]
 
-  colnames(statistika) <- c("Leto", "Krog", "Izbor", "Ime", "All-Pro", "Pro Bowl",
+  colnames(statistika) <- c("Leto", "Krog", "Izbor", "Ime", "All.Pro", "Pro.Bowl",
                           "St.tekem", "Uspesne.p", "Podaje",
                           "Jardi.podaje", "Podaje.TD", "Prestrezene.p",
                           "St.tekov","Jardi.tek", "TD.tek")
 
 
-
-  write_csv(statistika, "podatki/statistika.csv")
+  return(statistika)
   
 }
+statistika <- uvozi.statistiko()
 
-# 
-skupna <- merge(statistika, draft.ekipe, by = "Ime")
-skupna <- skupna[, c(1:2, 17:18, 3:16)]
+podaje.td <- statistika %>% select(Ime, Uspesne.p, Podaje, Podaje.TD, Prestrezene.p, TD.tek) %>%
+  melt(variable.name = "tip", value.name = "stevilo", na.rm = TRUE)
 
+jardi <- statistika %>% select(Ime, Podaje, Jardi.podaje, St.tekov, Jardi.tek) %>% 
+  melt(variable.name = "tip", value.name = "stevilo", na.rm = TRUE)
+
+tekme <- select(statistika, Ime, St.tekem)
+tekme[is.na(tekme)] <- 0
+
+pro <- tekme <- statistika %>% select(Ime, All.Pro, Pro.Bowl) %>% 
+  melt(variable.name = "tip", value.name = "stevilo", na.rm = TRUE) 
+
+# draft <- select(statistika, Ime, Leto, Krog, Izbor)
 
