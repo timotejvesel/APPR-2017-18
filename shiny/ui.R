63878babb647341f1d2c0ecb67dcaf07ca77ce89
Timotej Vesel <timotej.vesel@gmail.com>
2018-02-04 16:14:49 +0100

    Naredil shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,43 @@
 library(shiny)
 
-shinyUI(fluidPage(
+fluidPage(
+  titlePanel("Statistika"),
   
-  titlePanel("Slovenske občine"),
+  fluidRow(
+    column(4,
+      selectInput("leto",
+                  "Leto:",
+                  c("Vsa",
+                    unique(statistika$Leto)))
+    ),
+      column(4,
+             selectInput("krog",
+                         "Krog:",
+                         c("Vsi",
+                           sort(unique((statistika$Krog)))))
+      )
+  ),
+  fluidRow(
+    DT::dataTableOutput("tabela")
+  ),
   
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
-))
+  tabPanel("Graf",
+           
+           sidebarPanel(
+             selectInput("sprem", label="Izberi kategorijo",
+                         choices=colnames(povprecja[c(-1)]))
+           ),
+           mainPanel(plotOutput("graf1"))),
+  
+  tabPanel("Graf",
+           sidebarPanel(
+             selectInput("sprem2", label="Izberi kategorijo",
+                         choices=colnames(stat.tekma[c(-1,-2,-3,-4)]))
+           ),
+           mainPanel(plotOutput("graf2")))
+  
+             
+           )
+  
+
+
